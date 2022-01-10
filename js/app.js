64d66c892d378ee7934e9bd3a19d5a8ac37f3abe
volodymyr <v.ohlopkov100@gmail.com>
2022-01-10 21:10:01 +0200

    v 0.8
    section tokens were added + 3 subsections

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
+// MENU
 const menuBtn = document.querySelector('.header__burgerMenu'),
       menuNav = document.querySelector('.header__nav'),
-      closeMenu = document.querySelectorAll('.header__nav_links');
+      closeMenu = document.querySelectorAll('.header__nav_links'),
+      body = document.querySelector('body');
 
 menuBtn.addEventListener('click', (e)=> {
     e.target.classList.toggle('active');
     menuNav.classList.toggle('active');
+    body.classList.toggle('active');
 });
 
 closeMenu.forEach(el => el.addEventListener('click', ()=> {
     menuBtn.classList.remove('active');
     menuNav.classList.remove('active');
+    body.classList.remove('active');
   }));
+// FIXED MENU
+let waypoint = new Waypoint({
+    element: document.getElementById('cards'),
+    handler: function(direction) {
+      let header = document.getElementById('header');
+      if(direction === 'down') {
+        header.classList.add('active');
+      } else {
+        header.classList.remove('active');
+      }
+    }
+  });
